80cf33d37a38e36c7715fd825c2979d7bf40a75b
FaysalXulhash <faysalahammed80@gmail.com>
2022-06-01 23:30:49 +0600

    Lecture 6 fully implemented

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -29,7 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'myApp.apps.MyappConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
